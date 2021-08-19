9a84618460e2097f54e9447c73179620695e7b72
radeskia <radeskia331@gmail.com>
2021-08-19 08:38:13 +0200

    Console clearing some warnings

--- client/src/components/ui/Header.js
+++ client/src/components/ui/Header.js
@@ -1,5 +1,5 @@
 // import { Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 import { useState } from "react";
 import Logo from "../widgets/Logo";
--- client/src/components/widgets/Button.js
+++ client/src/components/widgets/Button.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Nav, Button } from "react-bootstrap";
+//import { Nav, Button } from "react-bootstrap";
 
 import "../../assets/styles/nav.css";
 const Buton = ({ btn, text, where }) => {
